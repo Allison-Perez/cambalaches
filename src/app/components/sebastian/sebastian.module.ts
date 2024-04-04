313660fd99084ca1203879b5be52549f6b27eb05
SebastianGs20 <sebastian.gonzales@soy.sena.edu.co>
2024-04-04 00:09:02 -0500

    Nose na

--- src/app/components/sebastian/sebastian.module.ts
+++ src/app/components/sebastian/sebastian.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { SebastianRoutingModule } from './sebastian-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RegisterComponent,
+    SebastianRoutingModule,
   ]
 })
 export class SebastianModule { }
