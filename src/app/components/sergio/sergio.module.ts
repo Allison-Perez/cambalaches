d9e326ff865be8017401ef6cdc21f5b4b69bfe4a
andreypa3zl0pez <andreypaez0@gmail.com>
2024-04-04 01:25:26 -0500

    login2

--- src/app/components/sergio/sergio.module.ts
+++ src/app/components/sergio/sergio.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { SergioRoutingModule } from './sergio-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    SergioRoutingModule
   ]
 })
 export class SergioModule { }
