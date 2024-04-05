d1aca39e546a8ad80a0b0412272e99a7f9903134
SebastianGs20 <sebastian.gonzales@soy.sena.edu.co>
2024-04-05 15:50:10 -0500

    ya

---
+++ src/app/components/sebastian/products/products.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../prodructs.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+
+  addToCart(product: any): void {
+    // Agrega el producto al carrito
+    this.productService.addToCart(product);
+    // Redirige al usuario al componente del carrito
+    this.router.navigate(['/cart']);
+  }
+}
