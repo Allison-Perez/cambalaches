bb8ac2f9bf1e706b13f4d4be0b6bae8e277d1d95
andreypa3zl0pez <andreypaez0@gmail.com>
2024-04-07 17:10:29 -0500

    diferent3s

--- src/app/components/sergio/sergio-routing.module.ts
+++ src/app/components/sergio/sergio-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-
+import { ContactanosComponent } from './contactanos/contactanos.component';
 const routes: Routes = [
-
-{ path: "login", component: LoginComponent, pathMatch: "full" },
+{ path: "contactanos", component: ContactanosComponent },
+{ path: "login", component: LoginComponent },
 ];
 
 @NgModule({
