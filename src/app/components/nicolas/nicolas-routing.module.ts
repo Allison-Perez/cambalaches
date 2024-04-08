2930744eb80b5d8fbf5cdf9fe66fa9cbcbad65ee
Allison Briñez <allison.mitchelle@gmail.com>
2024-04-08 12:56:47 -0500

    grafico categopria mas vendida

--- src/app/components/nicolas/nicolas-routing.module.ts
+++ src/app/components/nicolas/nicolas-routing.module.ts
@@ -5,13 +5,16 @@ import { UsersComponent } from './users/users.component';
 import { AddProductsComponent } from './add-products/add-products.component';
 import { NavComponent } from './nav/nav.component';
 import { AdminComponent } from './admin/admin.component';
+import { AdminEstadisticasComponent } from './admin-estadisticas/admin-estadisticas.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent},
   { path: 'perfil', component: UsersComponent},
   { path: 'agregarproductos', component: AddProductsComponent},
   { path: 'navegar', component: NavComponent},
-  {path: 'useradmin', component: AdminComponent}
+  {path: 'useradmin', component: AdminComponent},
+  {path: 'adminEstadisticas', component: AdminEstadisticasComponent},
+
 ];
 
 @NgModule({
